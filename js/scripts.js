f8c2b1203f5f661bd097d5548d28d39195e1bf8a
Andy Bateman <dandrewbateman@gmail.com>
2019-01-04 19:08:27 -0600

    RSVP

--- js/scripts.js
+++ js/scripts.js
@@ -185,23 +185,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Andy & Sarah's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Sept 28, 2019'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Sept 30, 2018 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'THE BLUFFS AT CONNER PRAIRIE 13400 Allisonville Rd, Fishers, IN 46038',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
+            description: "We can't wait to see you on our big day."
         }
     });
 
@@ -216,10 +216,10 @@ $(document).ready(function () {
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+            && MD5($('#invite_code').val()) !== '6b50a380b691dfec09c209018f6da5d3') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzcSfrS408V_62ezwEqtbOKq9xV9Z8FdCjKbzwrvQFqYGan2hc/exec', data)
                 .done(function (data) {
                     console.log(data);
                     $('#alert-wrapper').html('');
